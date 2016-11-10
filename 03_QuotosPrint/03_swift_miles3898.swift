3a9a3ca151dcc28479b5ebef7fcbad4beaf670dc
Seongmin Kim <miles3898@yahoo.com>
2016-11-11 01:59:59 +0900

    Create 03_swift_miles3898.swift

---
+++ 03_QuotosPrint/03_swift_miles3898.swift
@@ -0,0 +1,7 @@
+print("What is the quote? ", separator: "", terminator: "")
+let quote = readLine()
+
+print("Whot said it? ", separator: "", terminator: "")
+let who = readLine()
+
+print("\(who!) says, \"\(quote!)\"")
