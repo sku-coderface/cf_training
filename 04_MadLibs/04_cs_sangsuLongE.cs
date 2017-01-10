11e9dd86761cead385402261f8f214eeef86c892
better4636 <better4636@naver.com>
2017-01-10 22:37:51 +0900

    04_Clear

---
+++ 04_MadLibs/04_cs_sangsuLongE.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a noun: ");
+            string noun = Console.ReadLine();
+
+            Console.Write("Enter a verb: ");
+            string verb = Console.ReadLine();
+
+            Console.Write("Enter an adjective: ");
+            string adjective = Console.ReadLine();
+
+            Console.Write("Enter an adverb: ");
+            string adverb = Console.ReadLine();
+
+            Console.WriteLine("Do you {0} your {1} {2} {3}", verb, adjective, noun, adverb);
+        }
+    }
+}
