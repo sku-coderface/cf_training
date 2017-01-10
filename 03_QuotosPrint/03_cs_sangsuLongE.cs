0ef191225860e20725836bf5a78974e2ef91d433
better4636 <better4636@naver.com>
2017-01-10 16:49:49 +0900

    03_Clear

---
+++ 03_QuotosPrint/03_cs_sangsuLongE.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _03_cs_sangsuLongE_struct
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("What is the Quote ? ");
+            Quote.QUOTE = Console.ReadLine();
+            Console.Write("Who said it ? ");
+            Quote.Name = Console.ReadLine();
+
+            Console.WriteLine(Quote.Name + " says, " + "\"" + Quote.QUOTE + "\"");           
+        }
+    }
+    class Quote
+    {
+        public static string QUOTE { get; set; }
+        public static string Name { get; set; }
+    }
+}
