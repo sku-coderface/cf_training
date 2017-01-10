56f3bf1b2f0bef953deeaae67a75d3d3322664ea
better4636 <better4636@naver.com>
2017-01-10 16:14:38 +0900

    02_clear

---
+++ 02_LetterCount/02_cs_sangsuLongE.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02_cs_sangsuLongE
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("What is the input string? : ");
+            string input = Console.ReadLine();
+            Console.WriteLine(input + " has "+(input.Length-1)+"characters");
+        }
+    }
+}
